c8f2be2990f85fe7cf9bb93d6c099350669578c4
Lautaro Pereyra <hi@imlautaro.com>
2023-01-03 11:27:15 -0300

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,7 @@
+<template>
+	<button
+		class="bg-blue-600 duration-100 hover:bg-blue-500 font-medium px-4 py-2 rounded-xl text-white"
+	>
+		<slot />
+	</button>
+</template>
---
+++ src/composables/useHello.ts
@@ -0,0 +1,5 @@
+export default () => {
+	const message = ref('Hello')
+
+	return { message }
+}
